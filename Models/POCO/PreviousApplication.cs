37cfd99f88ea5fbfe9978b2f118a0f6db4b6fde6
Carl-Johan Johansson <carl-johan.johansson.9016@student.uu.se>
2023-04-28 13:44:37 +0200

    Previous Applications

--- Models/POCO/PreviousApplication.cs
+++ Models/POCO/PreviousApplication.cs
@@ -1,19 +1,19 @@
-﻿using OfficeOpenXml.Style;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication5.Models.POCO
 {
   public class PreviousApplication
   {
-    [Key, Column(Order = 0)]
     public int ApplicationId { get; set; }
-    [Key, Column(Order = 1)]
-    [MaxLength(255)]
     public string PreviousDnr { get; set; }
-    [ForeignKey(nameof(ApplicationId))]
+
+    [Required]
+    [ForeignKey("ApplicationId")]
     public virtual ApplicationAndEvaluation Application { get; set; }
-    [ForeignKey(nameof(PreviousDnr))]
-    public virtual ApplicationAndEvaluation PreviousApplication { get; set; }
+    
+    [Required]
+    [ForeignKey("PreviousDnr")]
+    public virtual ApplicationAndEvaluation PrevApplication { get; set; }
   }
 }
