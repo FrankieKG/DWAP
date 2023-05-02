3c2cf4b8d07462e093ed9c7becc84fc7527508eb
adamelsoudi <adamsirrety@gmail.com>
2023-05-02 10:01:11 +0200

    IRepo is updated

--- Models/IRepository.cs
+++ Models/IRepository.cs
@@ -3,8 +3,14 @@
 public interface IRepository
 {
     IQueryable<ApplicationAndEvaluation> ApplicationAndEvaluations { get; }
-    
-    
+    IQueryable<Organization> Organizations { get; }
+    IQueryable<Participant> Participants { get; }
+    IQueryable<Payment> Payments { get; }
+    IQueryable<PreviousApplication> PreviousApplications { get; }
+    IQueryable<Program> Programs { get; }
+    IQueryable<ReportAndReclaim> ReportAndReclaims { get; }
+    IQueryable<ScholarshipAndGrant> ScholarshipAndGrants { get; }
+
     Task ReadFile(IFormFile file);
 
 }
