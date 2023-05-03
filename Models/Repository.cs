5868094e01452b85467bf8936e2d3b49ba48e7c7
Carl-Johan Johansson <carl-johan.johansson.9016@student.uu.se>
2023-05-03 11:38:07 +0200

    Repository

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -1,6 +1,4 @@
-﻿namespace WebApplication5.Models;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using Microsoft.AspNetCore.Hosting;
 using System.Linq;
@@ -9,6 +7,10 @@ using Microsoft.AspNetCore.Identity;
 using System.Reflection.Metadata.Ecma335;
 using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
+using OfficeOpenXml;
+using System.IO;
+
+namespace WebApplication5.Models;
 
 public class Repository : IRepository
 {
@@ -33,6 +35,26 @@ public class Repository : IRepository
     //Reads data from an Excel file and converts it to fields in POCO classes
     public async Task ReadFile(IFormFile file)
     {
+        
+        //Open the Excel file using ExcelPackage
+        using (var excelPackage = new ExcelPackage(new MemoryStream(formFile.OpenReadStream())))
+        {
+            //Select the worksheet you want to read data from using Worksheets collection
+            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[1];
+    
+            //Read the data from the worksheet by looping through the rows and columns of the selected worksheet
+            for (int row = worksheet.Dimension.Start.Row + 1; row <= worksheet.Dimension.End.Row; row++)
+            {
+                ApplicationAndEvaluation newAppli = new ApplicationAndEvaluation(); //Create new instance of POCO class
+                newAppli.Field1 = worksheet.Cells[row, 1].Value.ToString(); //Map data to POCO properties
+                newAppli.Field2 = worksheet.Cells[row, 2].Value.ToString();
+                newAppli.Field3 = worksheet.Cells[row, 3].Value.ToString();
+        
+                //Write the data to your SQL database
+                // ...
+            }
+        }
+        
     }
     
     
