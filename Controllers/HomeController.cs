5a665fe64e70c71fe10756e1a1562c2e69783402
adamelsoudi <adamsirrety@gmail.com>
2023-05-05 16:44:51 +0200

    Fixat ny front

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,5 +34,10 @@ namespace WebApplication5.Controllers
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Privacy()
+    {
+      throw new NotImplementedException();
+    }
   }
 }
