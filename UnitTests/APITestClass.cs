22a77d172e5de74337bce880c4a3f0ec09fe01fa
Carl-Johan Johansson <carl-johan.johansson.9016@student.uu.se>
2023-05-10 13:43:39 +0200

    Test Class completed

--- UnitTests/APITestClass.cs
+++ UnitTests/APITestClass.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MyApi.Controllers;
-using MyApi.Models;
 using System.Collections.Generic;
 using System.Linq;
 using WebApplication5.Controllers;
@@ -17,11 +15,14 @@ namespace WebApplication5.UnitTests
         private readonly ApplicationDbContext context;
 
 
-        public APITestClass()
+        //KONSTRUKTOR
+        public APITestClass(ApplicationDbContext context)
         {
+
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseSqlServer("DefaultConnection")
             .Options;
+
             context = new ApplicationDbContext(options);
 
             controller = new APIController(context);
@@ -29,17 +30,18 @@ namespace WebApplication5.UnitTests
 
 
         [Fact]
-        public async Task TestGet(int id)
+        public void TestGet()
         {
+            // Arrange
+            var expectedOrganization = new Organization { OrganizationId = 8, OrganizationName = "Ekebygymnaiset" };
 
             // Act
-            var result = controller.Get(8);
-
+            var result = controller.Get(8) as JsonResult;
+            var actualOrganization = result.Value as Organization;
 
             // Assert
-            
-
-
+            Assert.Equal(expectedOrganization.OrganizationId, actualOrganization.OrganizationId);
+            Assert.Equal(expectedOrganization.OrganizationName, actualOrganization.OrganizationName);
         }
 
     }
