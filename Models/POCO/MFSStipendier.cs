2139f96c5f47ad76f9fdc2214d183587a91266ec
Franco_K <Franco.Kovacevic.8369@student.uu.se>
2023-05-18 11:20:22 +0200

    MFSStipendier

---
+++ Models/POCO/MFSStipendier.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication5.Model
+{
+  public class MFSStipendier
+  {
+    //ApplicationsAndEvaluations
+    public string Dnr { get; set; }
+    public string Period { get; set; }
+    public string ApplicationStatus { get; set; }
+    //Payments
+    public int? Total_Granted_Amount { get; set; }
+    public int? Total_Approved_Amount { get; set; }
+
+
+  }
+}
